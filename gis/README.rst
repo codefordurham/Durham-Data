c6d0b55e9b63f477ce3757d4a3daf15b125f256a
Colin Copeland <copelco@caktusgroup.com>
2014-09-14 13:55:42 -0400

    add school geojson files

--- gis/README.rst
+++ gis/README.rst
@@ -4,13 +4,25 @@ Durham GIS data
 To setup your local environment you should create a virtualenv and install the
 necessary requirements::
 
-    mkvirtualenv --python=/usr/bin/python3.3 durham-gis
+    mkvirtualenv --python=/usr/bin/python3.4 durham-gis
     $VIRTUAL_ENV/bin/pip install -r $PWD/requirements.txt
 
-Download a ArcGIS REST resource::
 
-    python download.py --indent=2 http://gisweb2.ci.durham.nc.us/arcgis/rest/services/DurhamMaps/Parcels/MapServer/1/query
+Schools
+-------
 
-Convert it to GeoJSON::
+Download Durham Public School data::
 
-    ogr2ogr -f GeoJSON geo.json arcgis.json OGRGeoJSON
+    export GISROOT=http://gisweb2.ci.durham.nc.us/arcgis/rest/services
+    python download.py --dest schools/schools.geojson $GISROOT/DurhamMaps/DPS_Schools/MapServer/0/query
+    python download.py --dest schools/districts-middle.geojson $GISROOT/DurhamMaps/DPS_Schools/MapServer/1/query
+    python download.py --dest schools/districts-high.geojson $GISROOT/DurhamMaps/DPS_Schools/MapServer/2/query
+    python download.py --dest schools/districts-elementary.geojson $GISROOT/DurhamMaps/DPS_Schools/MapServer/3/query
+    python download.py --dest schools/districts-elementary-year-round.geojson $GISROOT/DurhamMaps/DPS_Schools/MapServer/4/query
+    python download.py --dest schools/districts-middle-year-round.geojson $GISROOT/DurhamMaps/DPS_Schools/MapServer/5/query
+    python download.py --dest schools/districts-elementary-magnet-walk.geojson $GISROOT/DurhamMaps/DPS_Schools/MapServer/6/query
+    python download.py --dest schools/districts-holt-easley.geojson $GISROOT/DurhamMaps/DPS_Schools/MapServer/7/query
+    python download.py --dest schools/districts-elementary-student-assignment.geojson $GISROOT/DurhamMaps/DPS_ElementaryStudentAssignment/MapServer/4/query
+
+
+DPS_ElementaryStudentAssignment/MapServer/4
--- gis/download.py
+++ gis/download.py
@@ -5,18 +5,22 @@ ArcGIS REST query: http://resources.arcgis.com/en/help/rest/apiref/index.html?qu
 """
 #!/usr/bin/env python
 import argparse
+import itertools
 import json
 import logging
+import os
 import requests
-import itertools
+import subprocess
+import tempfile
 
 
-logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
-                    level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
 class ArcGISResource(object):
+
+    CONVERSION_MAP = {'.geojson': 'GeoJSON',
+                      '.shp': 'ESRI Shapefile'}
 
     def __init__(self, uri):
         self.uri = uri
@@ -31,10 +35,9 @@ class ArcGISResource(object):
 
     def get(self, query):
         """Request JSON data from ArcGIS REST resource."""
-        query['f'] = 'json'
-        if 'where' not in query:
-            query['where'] = '1=1'
-        r = requests.get(self.uri, params=query)
+        params = {'f': 'json', 'where': '1=1'}
+        params.update(query)
+        r = requests.get(self.uri, params=params)
         try:
             return r.json()
         except ValueError:
@@ -56,7 +59,7 @@ class ArcGISResource(object):
         logger.info('Total pages: {:,}'.format(len(pages)))
         for index, ids in enumerate(pages):
             where = 'ObjectId>={} AND ObjectId<={}'.format(ids[0], ids[-1])
-            query = {'outFields': '*', 'where': where}
+            query = {'outFields': '*', 'where': where, 'outSR': 4326}
             yield self.get(query)
 
     def objects(self, limit=None):
@@ -72,17 +75,39 @@ class ArcGISResource(object):
             objects['features'].extend(page['features'])
         return objects
 
+    def download(self, dest, limit=None):
+        _, ext = os.path.splitext(dest)
+        if os.path.exists(dest):
+            os.unlink(dest)
+        # create temporary file
+        logger.info("Total objects: {:,}".format(self.count()))
+        tmp = tempfile.NamedTemporaryFile('w+', suffix='.json', delete=False)
+        logger.debug("Created temporary JSON file {}".format(tmp.name))
+        try:
+            # write JSON
+            json.dump(self.objects(limit), tmp)
+            tmp.close()
+            # convert file
+            format_name = self.CONVERSION_MAP.get(ext, 'ESRI Shapefile')
+            cmd = 'ogr2ogr -f {} {} {} OGRGeoJSON'.format(format_name, dest,
+                                                          tmp.name)
+            subprocess.check_call(cmd, shell=True)
+        finally:
+            os.unlink(tmp.name)
+
 
 def main():
+    logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
+                        level=logging.DEBUG)
     parser = argparse.ArgumentParser()
     parser.add_argument('uri', help='REST URI')
-    parser.add_argument('--indent', type=int, help='JSON Indent')
+    parser.add_argument('--indent', type=int, help='JSON Indent', default=2)
     parser.add_argument('--limit', type=int, help='Object limit')
+    parser.add_argument('--dest', type=str, default='arcgis.geojson',
+                        help='Destination file name and format')
     args = parser.parse_args()
     resource = ArcGISResource(args.uri)
-    logger.info("Total objects: {:,}".format(resource.count()))
-    with open('arcgis.json', 'w') as outfile:
-        json.dump(resource.objects(args.limit), outfile, indent=args.indent)
+    resource.download(dest=args.dest)
 
 
 if __name__ == "__main__":
